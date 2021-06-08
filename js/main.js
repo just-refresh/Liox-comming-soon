6fe84c11227eb64cece6dd79dfecdcd1930f2995
justrefresh <github@just-refresh.de>
2021-06-08 17:19:27 +0200

    buttonFarbe /  consolelog Fun

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var consoleSignatureStyle = "font-size: 16px;" +
+  "background: linear-gradient(to right, #0044FF, #00C3FF);" +
+  "color: white;" +
+  "text-align: center;" +
+  "padding: 10px 15px;" +
+  "margin-top: 10px;" +
+  "width: 100%;" +
+  "border-radius: 8px;";
+
+var consoleSignatureText = "%cNice to see you here! 👋 \n We're always searching 🕵️ for advise 💁🏻, feedback 💡 or more Power 💪 for our great Team! 🏘️ \n 📥 Just get in contact: hello@liox.io";
+
+console.log(consoleSignatureText, consoleSignatureStyle);
