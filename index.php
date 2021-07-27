18a67d5abf4b18eddf9ab1043616397fcccb1ed6
just-refresh <github@just-refresh.de>
2021-07-27 11:25:32 +0200

    Matomo eingefügt.

--- index.php
+++ index.php
@@ -43,6 +43,22 @@
     <link rel="preload" href="fonts/Ubuntu/Ubuntu-Bold.ttf" as="font" type="font/ttf" crossorigin>
     <link rel="preload" href="fonts/Ubuntu/Ubuntu-Regular.ttf" as="font" type="font/ttf" crossorigin>
 
+    <!-- Matomo -->
+      <script type="text/javascript">
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//matomo.liox.io/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '1']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+      </script>
+    <!-- End Matomo Code -->
+
  </head>
 
  <body>
